c789db29c243d3654c3bc12254c4d8d42aea2595
Taher <taher.snoussi@esprit.tn>
2023-12-29 17:42:20 +0100

    landing page

--- the-bridge-front/src/component/layers/Banner.jsx
+++ the-bridge-front/src/component/layers/Banner.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import bg from "../../assets/bg.jpg";
 function Banner() {
   return (
     <div className="h-full w-full flex items-center justify-center relative ">
--- the-bridge-front/src/component/layers/NavLanding.jsx
+++ the-bridge-front/src/component/layers/NavLanding.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import logo from '../../assets/logo.png'
 
 function NavLanding() {
   return (
     <div className='flex items-start h-[70px] p-2'>
-        <img src={logo} className=' w-26 h-[60px]' alt="" />
+        <img src="https://9antra.tn/content/images/LogoBridge.png" className=' w-26 h-[60px]' alt="" />
     </div>
   )
 }
